a371e8d8d154ca5eb69d135864d88e0740a6afb9
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2024-10-17 10:48:05 +0100

    chore: add ansible playbooks

--- vagrant/guide.sh
+++ vagrant/guide.sh
@@ -4,8 +4,21 @@ gcloud auth login
 
 gcloud config set project agisit24-g10
 
+cd k8s
 terraform init
 
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
 
 terraform apply -auto-approve
+
+ansible-playbook ansible-gcp-configure-nodes.yml
+
+ansible-playbook ansible-k8s-install.yml
+
+ansible-playbook ansible-create-cluster.yml
+
+ansible-playbook ansible-workers-join.yml
+
+./scripts/fill-secrets.sh
+
+ansible-playbook ansible-start-deployment.yml
