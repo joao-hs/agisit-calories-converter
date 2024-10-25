9e48b56035e902229b553c558da656a5c8624bd0
Beatriz <beatriz.matias@tecnico.ulisboa.pt>
2024-10-25 05:44:12 +0100

    feat: Deploy Node Exporter

--- vagrant/deploy-guide.sh
+++ vagrant/deploy-guide.sh
@@ -16,6 +16,9 @@ terraform apply --auto-approve
 # CA private key and self-signed certificate
 openssl genrsa -out secrets/ca.key 2048
 openssl req -x509 -new -nodes -key secrets/ca.key -sha256 -days 3650 -out secrets/ca.crt -config config-files/ca.cnf
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz -O resources/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xvfz resources/node_exporter-1.8.2.linux-amd64.tar.gz -C resources/
+rm resources/node_exporter-1.8.2.linux-amd64.tar.gz
 
 ansible-playbook ansible-deploy-00-gcp-configure-nodes.yml
 
